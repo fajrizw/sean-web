4088e4c716464c9bf9a72eea78e5504d5510937a
fajrizw <fzhahiran@gmail.com>
2024-08-18 16:00:50 +0700

    add feature update and delete operation

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,65 @@ app.post("/", async (req, res) => {
     }
 });
 
+app.get("/post/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { data: post, error } = await db.from("post").select().eq("id", id);
+        if (error) throw error;
+        if (post.length === 0) {
+            return res.status(404).json({ error: "Post not found" });
+        }
+        res.json({ post: post[0] });
+    } catch (error) {
+        console.error("Error fetching post:", error);
+        res.status(500).json({ error: "Error fetching post" });
+    }
+});
+app.put("/update/:id", async (req, res) => {
+    try {
+        const {id} = req.params
+        const {title, description} = req.body
+       
+
+      // Validate input
+      if (!id || !title || !description) {
+        console.error("Invalid input:", { id, title, description });
+        return res.status(400).json({ error: "Invalid input" });
+    }
+    console.log("Update request received with:", { id, title, description });
+        const {data: updatePost, error} = await db.from("post").update({title, description}).eq("id", id).select()
+        if (error) {
+            console.error("Database error:", error);
+            throw error;
+        }
+
+        if (updatePost.length === 0) {
+            console.error("No post found with ID:", id);
+            return res.status(404).json({ error: "Post not found" });
+        }
+
+        console.log("Updated post:", updatePost);
+        res.status(200).json({ updatePost });
+    } catch (error) {
+        console.error("Error updating post:", error);
+        res.status(500).json({ error: "Error updating post" });
+    }
+})
+app.delete("/:id", async (req, res) => {
+    try {
+        const { id } = req.params; // Extract 'id' from params
+        const { data: deletedPost, error } = await db.from("post").delete().eq("id", id).select();
+         // Log hasil query
+        if (error) throw error;
+        console.log("Delete response data:", deletedPost); // Log data hasil delete
+        res.status(200).json({ deletedPost });
+    } catch (error) {
+        console.error("Error deleting post:", error);
+        res.status(500).json({ error: "Error deleting post" });
+    }
+});
+
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
